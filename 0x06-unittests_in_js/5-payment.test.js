3be01f5a9240aea10a2741d8b49ac8d87e7a6ba7
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-08-22 15:20:35 +0000

    upgrade5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+// 5-payment.test.js
+
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" and call console.log once', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+
+  it('should log "The total is: 20" and call console.log once', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+});
+
