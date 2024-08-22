6deb572fb88b242954c949b177c5495fb1f4924c
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-08-22 17:03:19 +0000

    update api

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,39 @@
+// api.test.js
+
+const request = require('request');
+const { expect } = require('chai');
+const app = require('./api.js');
+
+describe('Index page', () => {
+  it('should return status code 200', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return status code 200 when :id is a number', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return status code 404 when :id is NOT a number', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
+
